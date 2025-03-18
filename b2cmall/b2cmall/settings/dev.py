b738b6e00e63f877cf5363e9d0fca72ea210aed9
ShuTing <hellendjango@gmail.com>
2025-03-18 23:37:20 +0800

    連線到MySql資料庫

--- b2cmall/b2cmall/settings/dev.py
+++ b2cmall/b2cmall/settings/dev.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'b2cmall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',  # 本機資料庫
+        'PORT': 3306,  
+        'USER': 'hellen',  
+        'PASSWORD': 'hellen',  
+        'NAME': 'drf_mall'  
     }
 }
 
